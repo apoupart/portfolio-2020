331f946cc4a17e7b55816f80a0341fe17cce2d3a
Alexandre <alex.poupart@outlook.com>
2021-02-05 18:06:24 -0500

    Add more responsive items

--- components/knowledgeDetail/knowledgeDetail.jsx
+++ components/knowledgeDetail/knowledgeDetail.jsx
@@ -14,6 +14,7 @@ import {
   faVuejs,
 } from '@fortawesome/free-brands-svg-icons';
 import {
+  faCheckCircle,
   faCode,
   faDumbbell,
   faLaptopCode,
@@ -39,6 +40,7 @@ fontawesome.library.add(
   faVuejs,
   faCode,
   faLowVision,
+  faCheckCircle,
   faDumbbell
 );
 
