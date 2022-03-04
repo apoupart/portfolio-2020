146ba829b701154e197e2f3df3790f5e0c4573a7
Alexandre <alex.poupart@outlook.com>
2022-03-03 21:20:57 -0500

    update-to-next-12: Bump to nextjs 12.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   trailingSlash: true,
   target: 'serverless',
+  swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
